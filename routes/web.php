ea0f2fc8ee5bbdce5372311baa03483054a2a232
Clément <clement.delord@epitech.eu>
2022-05-09 11:53:08 +0200

    Auth Routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, 'index']);
+Route::get('random', [IndexController::class, 'random']);
 
-Route::get('/random', [IndexController::class, 'random']);
-
-Route::get('/bonjour', function () {
-    return 'Hello World';
-});
+Route::get('dashboard', [AuthController::class, 'dashboard']);
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::get('login-handle', [AuthController::class, 'login'])->name('login.handle');
+Route::get('registration', [AuthController::class, 'registration'])->name('registration');
+Route::get('registration-handle', [AuthController::class, 'handleRegistration'])->name('registration.handle');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
